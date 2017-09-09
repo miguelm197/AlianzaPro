1ef0403dcff21aa2c9a341fefab8e11d4acc4f0a
miguelm197 <miguelmax197@gmail.com>
2017-09-08 21:48:34 -0300

    b

--- AWD/Sitios/Sitio5/js/script.js
+++ AWD/Sitios/Sitio5/js/script.js
@@ -7,6 +7,18 @@ $(document).ready(function () {
         altura();
     })
 
+
+
+    $(window).scroll(function () {
+        
+    })
+
+    function moverSlider(numSlider) {
+        $('html, body').animate({
+            scrollTop: $(".slider" + numSlider).offset().top
+        }, 1000);
+    }
+
     function altura() {
         var altura = $(window).height();
         $(".slider").height(altura);
