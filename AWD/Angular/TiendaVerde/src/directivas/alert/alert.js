a3fd5dc6a4c93af360441b2c512238e41b4a716d
miguelm197 <miguelmax197@gmail.com>
2017-12-02 15:29:55 -0300

    Alert

---
+++ AWD/Angular/TiendaVerde/src/directivas/alert/alert.js
@@ -0,0 +1,10 @@
+app.directive('alert', [function () {
+    return {
+        templateUrl: './src/directivas/alert/alert.html',
+        restrict: 'E',
+        scope: {
+        },
+        link: function (scope, element) {
+        }
+    }
+}]);
